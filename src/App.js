4b395e8a1191cb2cf69b920d5a5208535a7b3989
Lam L <ppoo44ttaa@gmail.com>
2021-12-01 22:48:13 +0300

    feat: select size pizza   5

--- src/App.js
+++ src/App.js
@@ -10,12 +10,24 @@ import { Route } from 'react-router';
 
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('http://localhost:3000/bd.json')
+      .then((resp) => resp.json())
+      .then(json => {
+        setPizzas(json.pizzas);
+    });
+  }, []);
+
+  console.log(pizzas)
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route path="/" component={Home} exact/>
-        <Route path="/cart" component={Cart} exact/>
+        <Route path="/" render={() => <Home items={pizzas} />} exact/>
+        <Route path="/cart" render={() => <Cart />} exact/>
       </div>
     </div>
   );
