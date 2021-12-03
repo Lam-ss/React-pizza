ee69843328204899c3bf7e30535a3d13cfafc79f
Lam L <ppoo44ttaa@gmail.com>
2021-12-03 15:54:07 +0300

    feat: add thunk   7

--- src/App.js
+++ src/App.js
@@ -5,17 +5,15 @@ import { Route } from 'react-router';
 import axios from 'axios';
 import React, { Component, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
-import { setPizzas } from './redux/actions/pizzas';
+import { fetchPizzas } from './redux/actions/pizzas';
 
 
 function App() {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
   
 
   React.useEffect(() => {
-    axios.get('http://localhost:3001/pizzas').then(({ data }) => {
-      dispatch(setPizzas(data));
-    });
+    dispatch(fetchPizzas())
   }, [])
 
   return (
--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -1,3 +1,15 @@
+import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux';
+
+
+export const fetchPizzas = () => (dispatch) => {
+    axios.get('http://localhost:3001/pizzas').then(({ data }) => {
+        dispatch(setPizzas(data));
+    });
+}
+
+
+
 
 export const setPizzas = (items) => ({
     type: 'SET_PIZZAS',
